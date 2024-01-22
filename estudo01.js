da70aad2b1884fef59278d43866b264460c54ac6
Gleiton Brito Bernardes <154294770+Gleitonbb@users.noreply.github.com>
2024-01-21 22:43:06 -0300

    atualizações do Dom

---
+++ estudo01.js
@@ -0,0 +1,16 @@
+const fla = document.getElementById('fl')
+const pal = document.getElementById('pa')
+const sao = document.getElementById('sp')
+const cor = document.getElementById('co')
+const flu = document.getElementById('fu')
+const vas = document.getElementById('va')
+
+let times = [fla, pal, sao, cor, flu, vas]
+
+const tim = document.getElementsByTagName('div')
+
+console.log(times)
+console.log(tim)
+
+
+
